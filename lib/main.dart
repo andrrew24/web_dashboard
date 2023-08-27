d822b4a876416bb73561bb3384fd31594faf901d
Andrew <andrewwasfy24@gmail.com>
2023-08-27 16:54:15 +0300

    add custom theme

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const WebDashboard());
+}
+
+class WebDashboard extends StatelessWidget {
+  const WebDashboard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Dash",
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        textTheme:
+            GoogleFonts.mulishTextTheme(Theme.of(context).textTheme).apply(
+          bodyColor: Colors.black,
+        ),
+        pageTransitionsTheme: const PageTransitionsTheme(builders: {
+          TargetPlatform.iOS: FadeUpwardsPageTransitionsBuilder(),
+          TargetPlatform.android: FadeUpwardsPageTransitionsBuilder(),
+        }),
+        primaryColor: Colors.blue,
+      ),
+      
+    );
+  }
+}
